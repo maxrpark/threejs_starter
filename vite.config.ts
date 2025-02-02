62693dc306c7f3dd2cc8ba9fb1a08e3c24d505a0
maxirpark <maxirutipark@gmail.com>
2025-02-02 14:03:11 +0900

    update packages and added tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,6 @@
 import glsl from "vite-plugin-glsl";
-
-// const isCodeSandbox =
-//   "SANDBOX_URL" in process.env || "CODESANDBOX_HOST" in process.env;
+import tailwindcss from "@tailwindcss/vite";
 
 export default {
-  // root: 'src/',
-  // publicDir: "../static/",
-  // base: './',
-  // server:
-  // {
-  //     host: true,
-  //     open: !isCodeSandbox // Open if it's not a CodeSandbox
-  // },
-  // build:
-  // {
-  //     outDir: '../dist',
-  //     emptyOutDir: true,
-  //     sourcemap: true
-  // },
-  plugins: [glsl()],
+  plugins: [glsl(), tailwindcss()],
 };
